71aabdadb04885e7458e465e84bec83c0d8c328b
Patrick PREMARTIN <patrick.premartin@developpeur-pascal.fr>
2023-08-09 19:54:24 +0200

    updated the test project with its new folders tree

--- test-project/TestProject.dpr
+++ test-project/TestProject.dpr
@@ -4,9 +4,9 @@ uses
   System.StartUpCopy,
   FMX.Forms,
   fTestProject in 'fTestProject.pas' {Form2},
-  uProject in 'uProject.pas',
-  Olf.Net.Socket.Messaging in '..\src-library\Olf.Net.Socket.Messaging.pas',
-  Olf.RTL.Streams in '..\lib-externes\librairies\Olf.RTL.Streams.pas';
+  Olf.RTL.Streams in '..\lib-externes\librairies\Olf.RTL.Streams.pas',
+  Olf.Net.Socket.Messaging in '..\lib-externes\Socket-Messaging-Library\src\Olf.Net.Socket.Messaging.pas',
+  uProject in '..\src\uProject.pas';
 
 {$R *.res}
 
